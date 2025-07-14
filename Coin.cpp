6d79f1480f368d97eb881739bbf0e6af760144c0
Gavin Smith <131gms@gmail.com>
2025-07-14 10:41:45 -0500

    code

--- Coin.cpp
+++ Coin.cpp
@@ -1,1 +1,19 @@
 #include "Coin.h"
+#include <iostream>
+using namespace std;
+
+
+Coin::Coin() : centValue(1)
+{
+	isHeads = false;
+	
+}
+
+Coin::Coin(int value) centValue(value)
+{
+	isHeads = false;
+}
+void flip();
+const bool isItHeads();
+const string getSideUp();
+const int getStringValue();
--- Coin.h
+++ Coin.h
@@ -1,7 +1,21 @@
 #ifndef COIN
 #define COIN
+#include <iostream>
+using namespace std;
+
 class Coin
 {
+private:
+	bool isHeads;
+	const int centValue;
+public:
+	Coin();
+	Coin(int value);
+	void flip();
+	const bool isItHeads();
+	const string getSideUp();
+	const int getStringValue();
+
 
 };
 
