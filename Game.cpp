fbff2054eccee8eff83fc02c1d1f0490f4226756
Gavin Smith <131gms@gmail.com>
2025-07-14 12:59:45 -0500

    Finished game

--- Game.cpp
+++ Game.cpp
@@ -11,9 +11,6 @@ Game::Game()
 	totalScore = 0;
 	roundsPlayed = 0;
 
-	
-
-
 }
 
 Game::Game(double target)
@@ -33,11 +30,15 @@ Game::~Game()
 void Game::printFlipResults()
 {
 	
+	cout << "Nickel: " << coins[0].getSideUp() << endl;
+	cout << "Dime: " << coins[1].getSideUp() << endl;
+	cout << "Quarter: " << coins[2].getSideUp() << endl << endl;
 
 }
 
 void Game::flipCoins()
 {
+	cout << "Flip #" << getRounds() + 1 << endl;
 	for (int i = 0; i < numberOfCoins; i++)
 	{
 		coins[i].flip();
@@ -54,10 +55,42 @@ int Game::getScore()
 	return totalScore;
 }
 
+void Game::printScore()
+{
+	cout << totalScore << endl;
+}
+
 void Game::playGame()
 {
-
+	int play;
 	cout << "Welcome! You have 3 coins to flip (nickel, dime, and quarter) and your goal is to reach " << targetBalance
-		<< " in the shortest ammount of rounds. Good Luck!" << endl;
-	flipCoins();
+		<< " in the shortest ammount of rounds. Good Luck!" << endl << endl;
+	
+	do {
+		cout << "Enter 1 to flip." << endl;
+		cin >> play;
+		if (play != 1)
+		{
+			cout << "Try again" << endl;
+		}
+		cout << endl;
+
+		flipCoins();
+		printFlipResults();
+		roundsPlayed++;
+
+		for (int i = 0; i < numberOfCoins; i++)
+		{
+			if (coins[i].isItHeads())
+			{
+				totalScore += coins[i].getCentValue();
+			}
+		}
+
+	} while (totalScore < targetBalance);
+
+	cout << "Good Game!" << endl
+		<< "Score: ";
+	printScore();
+	cout << "Number of Rounds: " << roundsPlayed << endl;
 }
